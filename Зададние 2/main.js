9522267c67d370630eef5e9b5e8599b5066417af
Viktoriya Lebedeva <NikaLikeOne@yandex.ru>
2024-10-14 00:54:07 +0300

    commit

--- Зададние 2/main.js
+++ Зададние 2/main.js
@@ -2,25 +2,27 @@ $(document).ready(function () {
     $('#form').on('submit', function (e) {
         e.preventDefault();
 
-            const formData = new FormData(this);
+        let formData = new FormData(this);
 
-            const jsonObject = Object.fromEntries(Array.from(formData));
+        let jsonObject = Object.fromEntries(Array.from(formData));
 
-            const jsonString = JSON.stringify(jsonObject, null, 5);
-            
-            $('#output').text(jsonString);
+        let jsonString = JSON.stringify(jsonObject, null, 5);
 
-            $.ajax({
-                url: 'index.php',
-                type: 'GET',
-                data: $.param(jsonObject),
-                success: function(response) {
-                    alert('Ответ от сервера: ' + response);
-                },
-                error: function(jqXHR, textStatus, errorThrown) {
-                    alert('Ошибка: ' + textStatus);
-                }
-            });
+        let param = $.param(jsonObject)
+        
+        $('#output').text(jsonString);
 
+        $.ajax({
+            url: 'index.php',
+            type: 'GET',
+            datatype: 'text',
+            data: param,
+            success: function(response) {
+                alert('Ответ от сервера: ' + response);
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                alert('Ошибка: ' + textStatus);
+            }
+        });
     });
 }); 
